73022d6cccae2abe3ca4a2c0c5e5d5e74849a505
pzmrzy <pzmrzy@gmail.com>
2016-09-17 13:51:04 -0700

    solve 52.n queen II

---
+++ python/NQueen_II.py
@@ -0,0 +1,29 @@
+import Queue
+class Solution(object):
+    def check(self, grid, row):
+        n = len(grid)
+        for i in range(n):
+            if grid[i] == row:
+                return False
+            if (n - i) == abs(grid[i] - row):
+                return False
+        return True
+
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        N = n
+        q = Queue.LifoQueue()
+        q.put([])
+        res = []
+        while not q.empty():
+            now = q.get()
+            if len(now) == N:
+                res.append(now)
+                continue
+            for i in range(N):
+                if self.check(now, i):
+                    q.put(now+[i])
+        return len(res)
