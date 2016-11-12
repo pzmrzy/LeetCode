6bdfdd0db8bed05c9b272b450a78affb9a5ebc8e
pzmrzy <pzmrzy@gmail.com>
2016-11-12 10:11:05 -0800

    solve 244.shortest word distance II

---
+++ python/two_sum.py
@@ -0,0 +1,29 @@
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        n = len(nums)
+        for i in range(n):
+            ind.setdefault(nums[i], [])
+            ind[nums[i]].append(i)
+        nums.sort()
+        
+        for i in range(n - 1):
+            t = target - nums[i]
+            l = i + 1
+            r = n - 1
+            while l <= r:
+                mid = (l + r) / 2
+                if nums[mid] == t:
+                    if nums[i] == nums[mid]:
+                        return [ind[nums[i]][0], ind[nums[i]][1]]
+                    return [ind[nums[i]][0], ind[nums[mid]][0]]
+                elif nums[mid] < t:
+                    l = mid + 1
+                else:
+                    r = mid - 1
